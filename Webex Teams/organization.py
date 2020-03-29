1dec140c1ad4bd86d1668ae0c8fd454a48623048
chrissembiring <mail@chrissembiring.me>
2020-03-29 23:20:46 +0700

    Added organization API call

---
+++ Webex Teams/organization.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+access_token = ''
+
+url = 'https://api.ciscospark.com/v1/people'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+}
+
+params = {
+    'email': ''
+}
+
+res = requests.get(url, headers=headers, params=params)
+print(json.dumps(res.json(), indent = 4))
